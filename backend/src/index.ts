47889ea3f9daf7d784d7d0bc9cc1e24f67d730d4
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-07-15 09:26:39 +0530

    adding a signing route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,42 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
 
-const app = new Hono<{
-  Bindings: {
-    DATABASE_URL: string;
-  };
-}>();
+// const app = new Hono<{
+//   Bindings: {
+//     DATABASE_URL: string;
+//   };
+// }>();
+
+type Bindings = {
+  DATABASE_URL: string;
+  JWT_SECRET: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
 
 // cstand for context which means it has req, res and next in it
-app.post("/api/v1/signup", (c) => {
+app.post("/api/v1/signup", async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
-  return c.text("signup route");
+
+  const body = await c.req.json();
+
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    },
+  });
+  // json web token
+  const token = sign({ id: user.id }, c.env.JWT_SECRET);
+  return c.json({
+    jwt: token,
+  });
+
+  // return c.json(user);
 });
 
 app.post("api/v1/signin", (c) => {
